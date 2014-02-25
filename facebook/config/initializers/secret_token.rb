88ab97a9dbdac050b8a73b2a238db329aea4db48
Simha <simha.gemaraju@gmail.com>
2014-02-25 17:30:39 +0530

    initial commit-already functional :)

---
+++ facebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facebook::Application.config.secret_token = 'fb3f3d8d800b456f962d8c14853f20978b02b04707db940fcb99493e868b66e9a23425fb44a7cfca9d5607129d204384d9eec182abd6d0c6d18dc8c36f5ebd23'
